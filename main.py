d7b91f58f4ad7d48c7bb544e040c285283ba3153
Soman Singhal <somansinghal06@gmail.com>
2025-12-31 08:20:11 +0530

    Add number guessing game in main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+print("this is a number game")
+print("enter 1 to play or 2 to exit")
+num1=int(input('enter the number='))
+if num1==2:
+    exit()
+if num1==1:
+    print("lets start the game")
+print("ok!,so question1 on your screen....")
+print("what is the value of sin90")
+num2=int(input("enter the answer"))
+if num2==1:
+    print("wow!, you did it")
+else:
+    print("better luck next time")
+print("ok!,question 2 on your screen.....")
+print("what is the value of cos90")
+num3=int(input("enter the answer"))
+if num3==0:
+    print("wow!,you did it")
+else:
+    print("better luck next time")
+print("thanks for playing the game")
+print("to exit enter 2")
+num=int(input('enter the number'))
+if num==2:
+    exit()
